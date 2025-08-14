bcec2a6f9168fbf45e1b35ed36bf26e3470fe519
vittrok <vittrok@gmail.com>
2025-08-15 01:52:06 +0200

    0.0.5 restore original update-matches

--- matches/netlify-prod/netlify/functions/update-matches.js
+++ matches/netlify-prod/netlify/functions/update-matches.js
@@ -1,20 +1,7 @@
 // netlify/functions/update-matches.js (CommonJS)
 exports.handler = async function (event, context) {
   try {
-    await client.connect();
-
-    // Приклад: отримати всі матчі
-    const { rows: matches } = await client.query("SELECT * FROM matches ORDER BY match_date DESC");
-
-    // Приклад: оновити поле seen для певного матчу
-    // event.body очікує JSON { id: <match_id>, seen: true/false }
-    if (event.body) {
-      const { id, seen } = JSON.parse(event.body);
-      await client.query("UPDATE matches SET seen = $1 WHERE id = $2", [seen, id]);
-    }
-
-    await client.end();
-
+    // TODO: тут буде реальна логіка синку з Neon
     return {
       statusCode: 200,
       headers: { "content-type": "application/json" },
